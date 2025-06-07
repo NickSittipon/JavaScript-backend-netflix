1947c177fea26b6ce35b7ea3e4449d671cd53251
Nick Sittipon <sittipon5725@gmail.com>
2025-06-07 16:48:50 +0700

    Update tmdb.service.js

--- backend/services/tmdb.service.js
+++ backend/services/tmdb.service.js
@@ -2,17 +2,19 @@ import axios from "axios";
 import { ENV_VARS } from "../config/envVars.js";
 
 export const fetchFromTMDB = async (url) => {
-	const options = {
-		headers: {
-			accept: "application/json",
-			Authorization: "Bearer " + ENV_VARS.TMDB_API_KEY,
-		},
-	};
-	const response = await axios.get(url, options);
+	try {
+		const joinChar = url.includes("?") ? "&" : "?";
+		const fullUrl = `${url}${joinChar}api_key=${ENV_VARS.TMDB_API_KEY}`;
 
-	if (response.status !== 200) {
-		throw new Error("Failed to fetch data from TMDB" + response.statusText);
+		const response = await axios.get(fullUrl);
+
+		if (response.status !== 200) {
+			throw new Error("Failed to fetch data from TMDB: " + response.statusText);
+		}
+
+		return response.data;
+	} catch (error) {
+		console.error("Error in fetchFromTMDB:", error.message);
+		throw error;
 	}
-
-	return response.data;
 };
